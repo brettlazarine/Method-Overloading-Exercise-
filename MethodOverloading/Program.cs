a4c19e8924235dd5fcec8c68845c693bdef5a8dc
Brett Lazarine <jblazarine@gmail.com>
2023-01-30 21:32:28 -0600

    complete exercise

--- MethodOverloading/Program.cs
+++ MethodOverloading/Program.cs
@@ -2,9 +2,35 @@
 {
     public class Program
     {
-        static void Main(string[] args)
+        public static int Add(int x, int y)
         {
-            Console.WriteLine("Hello, World!");
+            return x + y;
+        }
+
+        public static decimal Add(decimal x, decimal y)
+        {
+            return x + y;
+        }
+
+        public static string Add(int x, int y, bool z)
+        {
+            if (z == true)
+            {
+                return (x + y != 1) ? $"{x + y} dollars" : $"{x + y} dollar";
+            }
+            else
+            {
+                return $"{x + y}";
+            }
+        }
+            static void Main(string[] args)
+            {
+            Console.WriteLine(Add(1, 2)); // int Add()
+            Console.WriteLine(Add(1.7m, 3.5m)); // decimal Add()
+            Console.WriteLine(Add(1, 2, true)); // Add(true)
+            Console.WriteLine(Add(1, 2, false)); // Add(false)
+            Console.WriteLine(Add(1, 0, true)); // Add(1 dollar)
         }
     }
-}
+    }
+
